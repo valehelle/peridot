9b650f93c4190f49c193deb25675b49a27d0411e
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2019-09-03 00:48:52 +0800

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Peridot.MixProject do
   def project do
     [
       app: :peridot,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
